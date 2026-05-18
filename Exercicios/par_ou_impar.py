86cedccee846071e0510c6f9f76a9b6795e2161d
Bowie <lorenalrn22@gmail.com>
2026-05-18 14:34:51 -0400

    Tranformando o exercico com função

--- Exercicios/par_ou_impar.py
+++ Exercicios/par_ou_impar.py
@@ -1,7 +1,12 @@
+def par_ou_impar(numero):
+
+  if numero % 2 == 0 :
+    return f"O número {numero} é Par!"
+
+  else:
+    return f"O número {numero} é Impar!"
+
 numero = int(input("Digite um número inteiro: "))
 
-if numero % 2 == 0 :
-  print(f"O número {numero} é Par!")
-
-else:
-  print(f"O número {numero} é Impar!")
+resultado = par_ou_impar(numero)
+print(resultado)
