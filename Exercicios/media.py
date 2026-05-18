e7dc682f4ce4b35798dc6ce067c6ef56c65ba5a0
Bowie <lorenalrn22@gmail.com>
2026-05-18 14:21:12 -0400

    Segundo exercicio media.py

---
+++ Exercicios/media.py
@@ -0,0 +1,11 @@
+'''(Média Simples)
+Um aluno tirou as seguintes notas em suas quatro provas de matemática: (7,0), (8,5), (6,0) e (9,5). Qual foi a sua média final?'''
+
+nota1 = 7.0
+nota2 = 8.5
+nota3 = 6.0
+nota4 = 9.5
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f"A média do aluno  foi de {media}! ")
