110f56ec7582ec554ffaee5d04033c1c44716085
Bowie <lorenalrn22@gmail.com>
2026-06-08 15:08:16 -0400

    Adicionei alguns prints para melhor visualização, após execução dos parâmetros.

--- Exercicios/lista_paises.py
+++ Exercicios/lista_paises.py
@@ -6,12 +6,20 @@
 - Mostre o total de países.'''
 
 lista_paises = ["Escócia", "Inglaterra", "Brasil", "Chile", "Grécia"]
+
 lista_paises.append("Colômbia")
+print("Após adicionar a Colômbia!")
 print(lista_paises)
+
 lista_paises.insert(0, "Estados Unidos")
+print("Após adicionar Estados unidos por índice.")
 print(lista_paises)
+
 lista_paises.remove("Chile")
+print("Após remover o Chile.")
 print(lista_paises)
+
 lista_paises.pop(0)
+print("Após remover Escócia por índice.")
 print(lista_paises)
 print(f"A quantidade de países depois de todos os parâmetros é: {len(lista_paises)}" )
