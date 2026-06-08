135c1cd01de1c9f94d230ff3fa64ab668ebc5d5f
Bowie <lorenalrn22@gmail.com>
2026-06-08 14:03:58 -0400

    Adicionei print no else: e outro e para informar fim do programa.

--- Exercicios/tirar_CNH.py
+++ Exercicios/tirar_CNH.py
@@ -4,4 +4,7 @@ if idade >= 18:
   print("Já pode tirar a CNH!")
   print("Digija com cuidado!")
 else:
-  print("Ainda não pode tirar a CNH, tente próximo ano! ")
+  print("Ainda não pode tirar a CNH.")
+  print("Volte quando tiver 18 anos!!")
+
+print("Fim do programa!!")
