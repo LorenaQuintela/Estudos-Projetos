7771f039e6a27da276458b7f5b22b02b837ff8f1
Bowie <lorenalrn22@gmail.com>
2026-06-08 14:00:26 -0400

    Criei exercício tirar_CNH.py

---
+++ Exercicios/tirar_CNH.py
@@ -0,0 +1,7 @@
+idade = int(input("Iorfome sua idade: "))
+
+if idade >= 18:
+  print("Já pode tirar a CNH!")
+  print("Digija com cuidado!")
+else:
+  print("Ainda não pode tirar a CNH, tente próximo ano! ")
