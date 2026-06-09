c9489f87448021df374128af7af721e6cc120eae
Bowie <lorenalrn22@gmail.com>
2026-06-09 13:55:30 -0400

    Exercicio usando dicionário com informações de carro.

---
+++ Exercicios/dicionario_info_carro.py
@@ -0,0 +1,10 @@
+'''Crie um dicionário que armazene as informações de um carro, como: marca,  modelo, cor e ano. Em seguida, exiba uma frase apresentando essas informações do carro, no seguinte formato:
+O carro é um MARCA MODELO do ano ANO.'''
+
+carro  = {
+  "marca": "Fiat",
+  "modelo": "Punto",
+  "ano": 2011
+}
+
+print(f"O carro é um {carro["marca"]} {carro["modelo"]} do ano {carro["ano"]}.")
