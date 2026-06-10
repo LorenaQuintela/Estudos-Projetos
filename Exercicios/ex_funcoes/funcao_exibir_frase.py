05da6636e99a3df71d5bc409138aeb2e97d7f260
Bowie <lorenalrn22@gmail.com>
2026-06-10 15:11:48 -0400

    Correção de print da função.

--- Exercicios/ex_funcoes/funcao_exibir_frase.py
+++ Exercicios/ex_funcoes/funcao_exibir_frase.py
@@ -1,4 +1,4 @@
 def exibir_frase(nome, altura, linguagem):
-  print(f"{nome} tem {altura}m e programa em {linguagem}.")
+  print(f"{nome} tem {altura}m e está estudando e programando em {linguagem}.")
 
 exibir_frase("Lorena", 1.64, "Python")
