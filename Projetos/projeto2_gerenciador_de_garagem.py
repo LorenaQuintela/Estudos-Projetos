fdec1ce686f0e099bef223f6c3e7a7e69da7fb59
Bowie <lorenalrn22@gmail.com>
2026-06-30 09:52:47 -0400

    Tentando fazer a estrutura.

--- Projetos/projeto2_gerenciador_de_garagem.py
+++ Projetos/projeto2_gerenciador_de_garagem.py
@@ -1,10 +1,31 @@
 # Crud de carros -> Gerenciador de Garagem que permita cadastrar, listar, editar, deletar e sair.
+def Cadastrar_carro(placa, cor, modelo, ano):
+  placa = input("Digite a placa: ")
+  cor = input("Digite a cor: ")
+  modelo = input("Digite o modelo: ")
+  ano = input("Digite o ano: ")
 
-print("--- GERENCIADOR DE GARAGEM ---")
+  return placa, cor, modelo, ano
+
+def Listar_carro(lista):
+  return lista
+
+def Editar_carro():
+  return
+
+def Deletar_carro(placa):
+  placa = input("Digite a placa: ")
+
+
+
+print("--- GERENCIADOR DE GARAGEM ---\n")
 print("1 - Cadastrar um carro")
 print("2 - Listar carros")
 print("3 - Editar carro")
 print("4 - Deletar carro")
-print(" 5 - Sair")
+print("5 - Sair")
 
 opção = input("Escolha uma opção: ")
+
+if opção == "1":
+  cadastrar_carro()
