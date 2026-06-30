67b80f0df9b3108e87cab94686fa20da6b6289a5
Bowie <lorenalrn22@gmail.com>
2026-06-30 09:30:57 -0400

    Criação do projeto 2 e menu de apresentação.

---
+++ Projetos/projeto2_gerenciador_de_garagem.py
@@ -0,0 +1,10 @@
+# Crud de carros -> Gerenciador de Garagem que permita cadastrar, listar, editar, deletar e sair.
+
+print("--- GERENCIADOR DE GARAGEM ---")
+print("1 - Cadastrar um carro")
+print("2 - Listar carros")
+print("3 - Editar carro")
+print("4 - Deletar carro")
+print(" 5 - Sair")
+
+opção = input("Escolha uma opção: ")
