d777d7fd11ef598f2ea98ac4bcfd412c2cef12b4
Bowie <lorenalrn22@gmail.com>
2026-07-02 09:53:46 -0400

    Englobando o menu em uma função, e criando while para opções.

--- Projetos/projeto2_gerenciador_de_garagem.py
+++ Projetos/projeto2_gerenciador_de_garagem.py
@@ -1,4 +1,31 @@
 # Crud de carros -> Gerenciador de Garagem que permita cadastrar, listar, editar, deletar e sair.
+def exibir_menu():
+  print("\n--- GERENCIADOR DE GARAGEM ---\n")
+  print("1 - Cadastrar um carro")
+  print("2 - Listar os carros existentes.")
+  print("3 - Editar um carro")
+  print("4 - Deletar um carro")
+  print("5 - Sair")
+
+while True:
+  exibir_menu()
+
+  opcao = input("Escolha uma opção: ")
+
+  if opcao == "1":
+    print("\nAinda desenvolvendo")
+  elif opcao == "2":
+    print("Ainda desenvolvendo")
+  elif opcao == "3":
+    print("Ainda desenvolvendo")
+  elif opcao == "4":
+    print("Ainda desenvolvendo")
+  elif opcao == "5":
+    print("\nEncerrando o Gerenciador de Garagem! Até mais.")
+    break
+  else:
+    print("Opção inválida!!")
+
 def Cadastrar_carro(placa, cor, modelo, ano):
   placa = input("Digite a placa: ")
   cor = input("Digite a cor: ")
@@ -8,6 +35,7 @@ def Cadastrar_carro(placa, cor, modelo, ano):
   return placa, cor, modelo, ano
 
 def Listar_carro(lista):
+  lista_carros = []
   return lista
 
 def Editar_carro():
@@ -16,16 +44,3 @@ def Editar_carro():
 def Deletar_carro(placa):
   placa = input("Digite a placa: ")
 
-
-
-print("--- GERENCIADOR DE GARAGEM ---\n")
-print("1 - Cadastrar um carro")
-print("2 - Listar carros")
-print("3 - Editar carro")
-print("4 - Deletar carro")
-print("5 - Sair")
-
-opção = input("Escolha uma opção: ")
-
-if opção == "1":
-  cadastrar_carro()
