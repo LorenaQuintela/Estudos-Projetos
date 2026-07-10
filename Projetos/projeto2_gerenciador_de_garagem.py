1e2cd4b866a3150f6953e57a24172341a1910c27
Bowie <lorenalrn22@gmail.com>
2026-07-10 15:19:05 -0400

    Tratamento na função editar, verificando se a placa atual é igual ou diferente da placa a ser editada.

--- Projetos/projeto2_gerenciador_de_garagem.py
+++ Projetos/projeto2_gerenciador_de_garagem.py
@@ -81,8 +81,7 @@ def editar_carro():
   print("\nPressione Enter para manter o valor atual.")
 
   nova_placa = input(f"Nova placa (atual:{carro_existente["placa"]}): ").strip()
-
-  if len(nova_placa) > 0:
+  if len(nova_placa) > 0 and (nova_placa != carro_existente["placa"]):
     if encontrar_carro(nova_placa) != None:
       print("Já existe um outro com essa placa.")
       return
