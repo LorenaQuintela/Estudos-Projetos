c12420192a1a400bde93e602b7993a9e1eac08c3
Bowie <lorenalrn22@gmail.com>
2026-09-02 15:26:45 -0400

    Adicionado argumento de data com biblioteca date time ao exercício.

--- Exercicios/biblioteca_datetime/codigo1-datetime.py
+++ Exercicios/biblioteca_datetime/codigo1-datetime.py
@@ -1,4 +1,5 @@
 # Vamos usar um exercício de média do aluno, e adaptar a biblioteca datetime no exercício.
+from datetime import date
 
 media = float(input("Média do aluno: "))
 
@@ -7,11 +8,22 @@ if media >= 7.0:
 elif (media >= 4.0) and (media < 7.0):
   print("Recuperação")
 
-  nota_recuperacao = float(input("Nota recuperação: "))
+  data_prazo = date(2026, 1, 31)
 
-  if nota_recuperacao >= 7.0:
-    print("Passou na recuperação.")
+  data_prova_str = input("Informe quando o aluno fez a prova? (formato dd/mm/aaaa): ")
+
+  formato_data = "%d/%m/%Y"
+  data_prova_date = date.strptime(data_prova_str, formato_data)
+
+  if data_prova_date <= data_prazo:
+    
+    nota_recuperacao = float(input("Nota recuperação: "))
+
+    if nota_recuperacao >= 7.0:
+      print("Passou na recuperação.")
+    else:
+      print("Reprovou na recuperação.")
   else:
-    print("Reprovou na recuperação.")
+    print("Prova feita fora do prazo. Aluno reprovado!")
 else:
   print("Reprovou direto.")
